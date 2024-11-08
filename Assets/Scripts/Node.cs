d35d695bf443830352431756b4b08d90fae69188
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-09 00:44:45 +0700

    Add Node.cs : Chứa dữ liệu cho Đất - A* Ver
    - Được gắn trong prefab Đất [Obj cha]
    - Chỉ có các biến của thuật toán A*

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+enum SoilType
+{
+    Good, //Đất tốt
+    Cracked, //Đất nứt nẻ
+    Polluted, // Đất Ô nhiễm
+    Radioactive // Đất phóng xạ
+}
+public class Node : MonoBehaviour
+{
+    public bool isObstacle;
+
+    public float gCost;
+    public float hCost;
+    public float FCost => gCost + hCost;
+
+    public Node prevNode;
+    public List<Node> neighbors = new List<Node>();
+}
