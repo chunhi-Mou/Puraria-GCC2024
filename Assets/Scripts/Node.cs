c9409cf3ee22e623e5c0e9f4c4de67634c846e38
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-09 21:29:00 +0700

    Update Node.cs : Chứa dữ liệu cho Đất - A* Ver
    - Thêm hướng các ô liền kề Node hiện tại
    - Được gắn trong prefab Đất [Obj cha]
    - Chỉ có các biến của thuật toán A*

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -11,6 +11,16 @@ enum SoilType
 }
 public class Node : MonoBehaviour
 {
+    public static readonly Vector2[] directions = new Vector2[]
+    {
+        new Vector2(1, 0),  // Phải
+        new Vector2(-1, 0), // Trái
+        new Vector2(-1, -1), //Dưới-Trái
+        new Vector2(1, 1), // Trên-Phải
+        new Vector2(0, 1), // Trên-Trái
+        new Vector2(0, -1) //Dưới-Phải
+    };
+
     public bool isObstacle;
 
     public float gCost;
