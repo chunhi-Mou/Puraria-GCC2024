9504f989a0b9f4d10fbc3a247bb62204cc6cd28a
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-09 21:39:23 +0700

    scripts: Grid Map [Controller & View & Editor]
    - Controller: xử lí Logic sinh và update danh sách liền kề của các tâm node
    - View: Sinh (Đặt tên + Set sorting Order) và huỷ GameObject khi tạo mới map
    - Editor: Hiện Nút Gen Map ở Editor

---
+++ Assets/Scripts/GridMapView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMapView : MonoBehaviour
+{
+    public void ClearOldPrefabs()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+    public Node CreateNode(int x, int y, Vector2 position, GameObject prefab)
+    {
+        if (prefab != null)
+        {
+            GameObject tile = Instantiate(prefab, position, Quaternion.identity, transform);
+
+            tile.name = "Đất" + "(" + x + ", " + y + ")";
+            Node node = tile.GetComponent<Node>();
+
+            Renderer renderer = tile.GetComponentInChildren<SpriteRenderer>();
+            if (renderer != null)
+            {
+                renderer.sortingOrder = -(int)(y * 1000) + x;
+            }
+            return node;
+        }
+        return null;
+    }
+}
