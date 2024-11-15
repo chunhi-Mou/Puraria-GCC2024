964fc655a2812bcf515e43571ab36d49c1ec5a8e
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-16 06:59:03 +0700

    Update PathFindingAStar.cs

--- Assets/Scripts/PathFindingAStar.cs
+++ Assets/Scripts/PathFindingAStar.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PathFindingAStar : MonoBehaviour
 {
