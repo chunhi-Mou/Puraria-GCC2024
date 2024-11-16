9ae7952767955bcc584fcf20a4a39620550b4a79
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-16 17:01:36 +0700

    Update SoilBehaviour.cs
    - Để tránh conflict với khi bấm vào UI

--- Assets/Scripts/SoilBehaviour.cs
+++ Assets/Scripts/SoilBehaviour.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SoilBehaviour : MonoBehaviour
 {
     private void OnMouseDown() {
-        if(!PlayerView.isMoving)
+            if (!PlayerView.isMoving && !EventSystem.current.IsPointerOverGameObject())
             this.SetAsTarget();
     }
 
