09e1e42389f3b25aa2d123287aaaf5851ebbe607
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2024-11-20 10:30:36 +0700

    Update cs: Thêm RequireComponent
    - Require Node
    - Require PolygonCollider2D

--- Assets/Scripts/SoilBehaviour.cs
+++ Assets/Scripts/SoilBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Node))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class SoilBehaviour : MonoBehaviour
 {
     private void OnMouseDown() {
